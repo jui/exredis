6c47c5b74516b4af6dac85d7fc696f0913285560
Jussi Uusi-Illikainen <jui@kepit.fi>
2014-11-02 17:19:58 +0200

    Pool support

--- lib/exredis_pool.ex
+++ lib/exredis_pool.ex
@@ -18,7 +18,39 @@ defmodule Exredis.Pool do
              ]
     supervise(children, strategy: :one_for_one)
   end
-	
+
+  @doc """
+  Make query
+
+  * `query(client, ["SET", "foo", "bar"])`
+  * `query(client, ["GET", "foo"])`
+  * `query(client, ["MSET" | ["k1", "v1", "k2", "v2", "k3", "v3"]])`
+  * `query(client, ["MGET" | ["k1", "k2", "k3"]])`
+
+  See more commands in official Redis documentation
+  """
+  @spec query(list) :: any
+  def query(command) when is_pid(client) and is_list(command), do:
+    client = :poolboy.checkout(:exredis_pool)
+    ret = client |> :eredis.q(command) |> elem 1
+    :poolboy.checkin(:exredis_pool, client)
+    ret
+
+  @doc """
+  Pipeline query
+
+  ```
+  query_pipe(client, [["SET", :a, "1"],
+                      ["LPUSH", :b, "3"],
+                      ["LPUSH", :b, "2"]])
+  ```
+  """
+  @spec query_pipe(pid, list) :: any
+  def query_pipe(client, command) when is_pid(client) and is_list(command), do:
+    client = :poolboy.checkout(:exredis_pool)
+    ret = client |> :eredis.qp command
+   :poolboy.checkin(:exredis_pool, client)
+   ret
     
 end
 
